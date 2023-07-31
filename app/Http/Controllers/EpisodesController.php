42ab61e6f1682a444754b9ffde1a5ecc72be019e
GuilhermeCasagrandeF <118361478+GuilhermeCasagrandeF@users.noreply.github.com>
2023-07-31 19:06:12 -0300

    Marcar episodes como assistidos

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Episode;
 use App\Models\Season;
+use Illuminate\Http\Request;
 
 class EpisodesController
 {
     public function index(Season $season)
     {
-        return view('episodes.index', ['episodes' => $season->episodes]);
+        return view('episodes.index', ['episodes' => $season->episodes()->orderBy('id')->get()]);
+    }
+
+    public function update(Request $request, Season $season)
+    {
+        $watechedEpisodes = $request->episodes;
+        $season->episodes->each(function(Episode $episode) use ($watechedEpisodes) {
+            $episode->watched = in_array($episode->id, $watechedEpisodes);
+        });
+
+        $season->push();
+
+        return to_route('episodes.index', $season->id);
     }
 }
